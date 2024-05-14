cbe56a2fb5f36a2f8b76f5740897ce34cd617cb6
bakwarteng <bakwarteng@hotmail.com>
2024-05-14 11:15:21 -0500

    added .env file, working GET request for all animals

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,4 +1,4 @@
-//get requests 
+//get requests
 //where we create elements on the page
 
 //get all animals for homepage
@@ -7,3 +7,12 @@
 
 //get for login route
 
+const router = require("express").Router();
+const { Router } = require("express");
+const { Animals } = require("../models");
+
+router.get("/", async (req, res) => {
+  const allAnimalData = await Animals.findAll();
+  res.json(allAnimalData);
+});
+module.exports = router;
