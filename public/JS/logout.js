b66fc1baa43e8075f54982ec539d8c955aa425cb
Max Bonetti <maxwell.bonetti@gmail.com>
2024-05-15 10:33:12 -0500

    deleted extra logout function

--- public/JS/logout.js
+++ public/JS/logout.js
@@ -12,20 +12,5 @@ const logout = async () => {
   };
   
   document.querySelector('#logout').addEventListener('click', logout);
-  
 
-  const logout = async () => {
-    const response = await fetch('/users/logout', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-    });
-  
-    if (response.ok) {
-      document.location.replace('/');
-    } else {
-      alert('Failed to log out.');
-    }
-  };
-  
-  document.querySelector('#logout').addEventListener('click', logout);
   
