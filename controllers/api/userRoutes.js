247e6d438b3a5b88881a5e0031dfdff3848a35b9
Max Bonetti <maxwell.bonetti@gmail.com>
2024-05-15 10:31:59 -0500

    save and added last cat picture

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { User, Animals } = require('../../models');
-
+// /api/users
 //register a new user
 router.post('/', async (req, res) => {
     try {
